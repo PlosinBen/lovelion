847044763fdd6dd2d5c86e48537d7ccc8335a4c3
Ben <PlosinBen@gmail.com>
2020-10-18 18:07:54 +0800

    modify ledger header

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,23 +4,24 @@
 
 @section('Content')
     <section class="container">
-        <h5 class="h3">
+        <h4 class="h3">
             近期帳本清單
             <a id="createLedger" href="{{ route('bookkeeping.create') }}" class="btn btn-sm btn-success">新增</a>
-        </h5>
+        </h4>
 
         <div class="row">
             @foreach($ledgerEntities as $ledgerEntity)
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                     <a href="#">
-                        <div class="card">
+                        <div class="card ledger-card">
+                            <div class="card-header">
+                                {{ $ledgerEntity->name }}
+                            </div>
+                        <!--
                             <img src="{{ asset('img/blank.jpg') }}" class="card-img-top" alt="...">
+                        -->
                             <div class="card-body">
-                                <div class="ledger-card p-2">
-                                    <div class="name">
-                                        <label>名稱：</label>
-                                        <span class="float-right">{{ $ledgerEntity->name }}</span>
-                                    </div>
+                                <div class="p-2">
                                     <div class="currency">
                                         <label>幣別：</label>
                                         <span class="float-right">{{ $ledgerEntity->currency_code }}</span>
@@ -37,29 +38,6 @@
                     </a>
                 </div>
             @endforeach
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <a href="#">
-                    <div class="card">
-                        <img src="{{ asset('img/blank.jpg') }}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <div class="ledger-card p-2">
-                                <div class="name">
-                                    <label>名稱：</label>
-                                    <span class="float-right">2020</span>
-                                </div>
-                                <div class="currency">
-                                    <label>幣別：</label>
-                                    <span class="float-right">TWD</span>
-                                </div>
-                                <div class="expenses">
-                                    <label>總支出：</label>
-                                    <span class="float-right">{{ number_format(123345, 2) }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
         </div>
     </section>
 @endsection
@@ -75,8 +53,8 @@
             $('#createLedgerModal').modal()
         })
 
-        $('#createLedgerModal form').on('submit', function() {
-            if( $(this).find('input[name="name"]').val().length === 0 ) {
+        $('#createLedgerModal form').on('submit', function () {
+            if ($(this).find('input[name="name"]').val().length === 0) {
                 return false
             }
 
